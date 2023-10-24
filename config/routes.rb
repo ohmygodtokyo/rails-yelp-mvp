647dcb2d1d0800db6bb592407dd4323723083e51
Michael Nhye <Michael.Nhye@outlook.com>
2023-10-25 00:40:13 +0200

    Created a Yelp review mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root 'restaurants#index'
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "/restaurants", to: "restaurants#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+pop_ups = {name: "Pop Ups", address: "Waagner-Biro-Straße 109, 8020 Graz", phone_number: "0316573357", category: "belgian"}
+calamar = {name: "Calamar", address: "Waagner-Biro-Straße 47, 8020 Graz", phone_number: "06766829471", category: "italian"}
+adrial_grill = {name: "Adria Grill", address: "Wiener Str. 2, 8020 Graz", phone_number: "06765700940", category: "french" }
+restaurant_shanghai = {name: "Restaurant Shanghai", address: "Jasomirgottstraße 6, 1010 Wien", phone_number: "015337419", category: "chinese"}
+restaurant_kojiro = {name: "Restaurant Kojiro", address: "Gumpendorfer Str. 59, 1060 Wien", phone_number: "015337419", category: "japanese"}
+
+[pop_ups, calamar, adrial_grill, restaurant_shanghai, restaurant_kojiro].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
